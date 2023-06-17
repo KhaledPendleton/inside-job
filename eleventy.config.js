d94c499d93557532aa222be2ac6e0a2428a3afd1
Khaled Pendleton <hi@khaledpendleton.com>
2023-06-17 01:40:40 -0400

    Added collections for roles and organizations

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,16 @@
 module.exports = (eleventyConfig) => {
+    eleventyConfig.addCollection('roles', (collection) => {
+        return collection
+            .getFilteredByGlob('./src/roles/*/*.md')
+            .filter(({ page }) => page.fileSlug !== 'organization');
+    });
+
+    eleventyConfig.addCollection('organizations', (collection) => {
+        return collection.getFilteredByGlob('./src/roles/*/organization.md');
+    });
+
+    eleventyConfig.addPassthroughCopy('./src/assets/css/*.css');
+
     return {
         markdownTemplateEngine: 'njk',
         htmlTemplateEngine: 'njk',
